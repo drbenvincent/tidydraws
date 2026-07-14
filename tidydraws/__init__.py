b210198f45a95b87490284748aac69c886c5a4b4
Benjamin T. Vincent <inferencelab@gmail.com>
2026-07-14 14:52:12 +0100

    Bump version 0.4.1 -> 0.4.2

--- tidydraws/__init__.py
+++ tidydraws/__init__.py
@@ -20,7 +20,7 @@
 
 """tidydraws — A tidybayes-inspired data layer for declarative Bayesian visualisation in Python."""
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 from ._extract import parameter_draws as parameter_draws
 from ._extract import prediction_draws as prediction_draws
 from ._extract import compare_draws as compare_draws
